1a58b6a4d36a103ac4c0c826011233ca17311a10
Someguy8315 <dimadragos17@gmail.com>
2022-10-18 11:14:14 +0300

    add cross-platform setup scripts

---
+++ configure.ps1
@@ -0,0 +1,11 @@
+if ($IsWindows) {
+    $generator = "Visual Studio 17 2022"
+} else {
+    $generator = "Ninja Multi-Config"
+}
+
+$build_path = "build"
+
+if (!(Test-Path -Path $build_path)) {
+    cmake -G $generator -B $build_path
+}
