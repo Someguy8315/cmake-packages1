d342fe4a3b9ad44cebb178ae928e8eecb4e5f9e1
Someguy8315 <dimadragos17@gmail.com>
2022-12-27 14:00:21 +0200

    add division module

---
+++ ext/math_lib/modules/division/division.h
@@ -0,0 +1,6 @@
+#ifndef MATH_DIVISION_H
+#define MATH_DIVISION_H
+
+double divide(double a, double b);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include "math_lib.h"
+#include "division.h"
 
 int main()
 {
     double value1 = add(3.1, 2.5);
     double value2 = sub(10.2, 5.3);
+    double value3 = divide(2.0, 4.0);
 
     printf("value1 = %lf\n", value1);
     printf("value2 = %lf\n", value2);
+    printf("value3 = %lf\n", value3);
     return 0;
 }
